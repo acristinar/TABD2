f15ac97552adf781acfce910f6fc0843950b631f
villaca <danielvillacasilva@gmail.com>
2014-09-19 17:44:45 -0300

    Revert "Corrigindo disposição das pastas"
    This reverts commit bf1bf9a42171e981e999c1489d301808fd940301.

--- src/java/br/unirio/tabd2/dao/FuncionarioDAO.java
+++ src/java/br/unirio/tabd2/dao/FuncionarioDAO.java
@@ -1,13 +1,7 @@
 package br.unirio.tabd2.dao;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -27,33 +21,6 @@ public class FuncionarioDAO {
             e.printStackTrace();
         }
     
-    }
-    
-    public List<Funcionario> listarFuncionario() throws SQLException{
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-        List<Funcionario> listaFuncionario = new ArrayList<Funcionario>();
-        Funcionario funcionario = null;
-        
-        try{
-            abrirConexao();
-            String sql = "SELECT * FROM Funcionario";
-            stmt = conn.prepareStatement(sql);
-            rs = stmt.executeQuery();
-            while(rs.next()){
-                funcionario = new Funcionario();
-                funcionario.setCpf(rs.getInt("cpf"));
-                funcionario.setNome(rs.getString("nome"));
-                funcionario.setTipo(rs.getString("tipo"));
-                listaFuncionario.add(funcionario);
-            }
-        }catch(Exception e){
-            System.out.println("Erro ao abrir conexão: "+ e.getMessage());       
-        }finally{
-            //adicionar fecharConexao() depois
-        }
-        
-      return listaFuncionario;  
     }
     
 }
